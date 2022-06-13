d25c55863fb72d32e7a4118da431c8bb7573b721
Ice3man <nizamulrana@gmail.com>
2022-06-13 16:25:14 +0530

    Added scope manager

---
+++ pkg/utils/scope_test.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestScopeManagerValidate(t *testing.T) {
+	manager, err := NewScopeManager([]string{`google\..*`, `192\.168\.1\.1`}, []string{`uber\..*`}, true)
+	require.NoError(t, err, "could not create scope manager")
+
+	parsed, _ := url.Parse("https://google.com")
+	validated, err := manager.Validate(parsed)
+	require.NoError(t, err, "could not validate url")
+	require.True(t, validated, "could not get correct in-scope validation")
+
+	parsed, _ = url.Parse("https://test.google.com")
+	validated, err = manager.Validate(parsed)
+	require.NoError(t, err, "could not validate url")
+	require.True(t, validated, "could not get correct in-scope validation")
+
+	parsed, _ = url.Parse("https://uber.com")
+	validated, err = manager.Validate(parsed)
+	require.NoError(t, err, "could not validate url")
+	require.False(t, validated, "could not get correct out-scope validation")
+
+	parsed, _ = url.Parse("https://192.168.1.1")
+	validated, err = manager.Validate(parsed)
+	require.NoError(t, err, "could not validate url")
+	require.True(t, validated, "could not get correct in-scope ip validation")
+}
