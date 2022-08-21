c999fba1d6372cc355a7d9e400790734dd6d2958
Ice3man <nizamulrana@gmail.com>
2022-08-22 03:41:17 +0530

    Added separate domain and url scope options (#11)
    * Added non-headless content parsers based on security-crawl-project
    
    * Added output + misc options + cli beginning
    
    * Started runner creation + misc additions to parsers
    
    * Added scope manager
    
    * Added extension based validators
    
    * Added initial CLI implementation + misc
    
    * Misc improvements
    
    * Fixed a crash due to nil response
    
    * Misc fixes to output
    
    * Added banner + misc
    
    * Misc fixes + added version + misc
    
    * Misc fixes
    
    * misc
    
    * Added separate domain and url scope options
    
    * Added command line options

--- pkg/utils/scope/scope.go
+++ pkg/utils/scope/scope.go
@@ -14,11 +14,13 @@ import (
 type Manager struct {
 	inScope           []*regexp.Regexp
 	outOfScope        []*regexp.Regexp
+	inScopeDomains    []*regexp.Regexp
+	outOfScopeDomains []*regexp.Regexp
 	includeSubdomains bool
 }
 
 // NewManager returns a new scope manager for crawling
-func NewManager(inScope, outOfScope []string, includeSubdomains bool) (*Manager, error) {
+func NewManager(inScope, outOfScope, inScopeDomains, outOfScopeDomains []string, includeSubdomains bool) (*Manager, error) {
 	manager := &Manager{includeSubdomains: includeSubdomains}
 
 	for _, regex := range inScope {
@@ -35,6 +37,20 @@ func NewManager(inScope, outOfScope []string, includeSubdomains bool) (*Manager,
 			manager.outOfScope = append(manager.outOfScope, compiled)
 		}
 	}
+	for _, regex := range inScopeDomains {
+		if compiled, err := regexp.Compile(regex); err != nil {
+			return nil, fmt.Errorf("could not compile regex %s: %s", regex, err)
+		} else {
+			manager.inScopeDomains = append(manager.inScopeDomains, compiled)
+		}
+	}
+	for _, regex := range outOfScopeDomains {
+		if compiled, err := regexp.Compile(regex); err != nil {
+			return nil, fmt.Errorf("could not compile regex %s: %s", regex, err)
+		} else {
+			manager.outOfScopeDomains = append(manager.outOfScopeDomains, compiled)
+		}
+	}
 	return manager, nil
 }
 
@@ -42,48 +58,81 @@ func NewManager(inScope, outOfScope []string, includeSubdomains bool) (*Manager,
 func (m *Manager) Validate(URL *url.URL) (bool, error) {
 	hostname := URL.Hostname()
 
-	var subdomain string
-	var domain string
-	parsed := net.ParseIP(hostname)
-	if parsed != nil {
-		domain = hostname
-	} else {
-		var err error
-		domain, err = publicsuffix.EffectiveTLDPlusOne(hostname)
-		if err != nil {
-			return false, fmt.Errorf("could not parse domain %s: %s", hostname, err)
-		}
-		subdomain = strings.TrimSuffix(hostname, domain)
-	}
+	// If we have URL rules also consider them, if not just work on domain scope
+	inScopeURLs := len(m.inScope) > 0 && len(m.outOfScope) > 0
 
-	if len(m.outOfScope) > 0 {
-		var outOfScopeMatched bool
-		for _, item := range m.outOfScope {
-			if item.MatchString(domain) {
-				outOfScopeMatched = true
-			}
+	var validated, defaultMatch bool
+	var err error
+	if len(m.inScopeDomains) > 0 || len(m.outOfScopeDomains) > 0 {
+		validated, defaultMatch, err = m.validateHostname(hostname)
+		if !inScopeURLs {
+			return validated, err
 		}
-		if outOfScopeMatched {
+		if !defaultMatch {
+			return validated, err
+		}
+	}
+	validated, err = m.validateURL(URL.String())
+	return validated, err
+}
+
+func (m *Manager) validateURL(URL string) (bool, error) {
+	for _, item := range m.outOfScope {
+		if item.MatchString(URL) {
 			return false, nil
 		}
 	}
+
+	var inScopeMatched bool
 	if len(m.inScope) > 0 {
-		var inScopeMatched bool
 		for _, item := range m.inScope {
-			if item.MatchString(domain) {
+			if item.MatchString(URL) {
 				inScopeMatched = true
+				break
 			}
 		}
 		if !inScopeMatched {
 			return false, nil
 		}
-		if subdomain != "" {
-			if m.includeSubdomains {
-				return true, nil
-			}
-			return false, nil
-		}
-		return true, nil
 	}
 	return true, nil
+}
+
+func (m *Manager) validateHostname(hostname string) (bool, bool, error) {
+	var subdomain, domain string
+	var err error
+
+	parsed := net.ParseIP(hostname)
+	if parsed == nil {
+		domain, err = publicsuffix.EffectiveTLDPlusOne(hostname)
+		if err != nil {
+			return false, false, fmt.Errorf("could not parse domain %s: %s", hostname, err)
+		}
+		subdomain = strings.TrimSuffix(hostname, domain)
+	} else {
+		domain = hostname
+	}
+
+	for _, item := range m.outOfScopeDomains {
+		if item.MatchString(domain) {
+			return false, false, nil
+		}
+	}
+	var inScopeMatched bool
+	for _, item := range m.inScopeDomains {
+		if item.MatchString(domain) {
+			inScopeMatched = true
+			break
+		}
+	}
+	if !inScopeMatched {
+		return false, false, nil
+	}
+	if subdomain != "" {
+		if m.includeSubdomains {
+			return true, false, nil
+		}
+		return false, false, nil
+	}
+	return true, true, nil
 }
