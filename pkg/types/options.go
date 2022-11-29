8f9461b561f62fa6791d5ca8feea7f367dc5ecb5
Shubham Rasal <shubhamdharmarasal@gmail.com>
2022-11-29 18:40:08 +0530

    Add katana as lib (#205)
    * Add katana as lib
    
    * Add callback function
    
    * Close the crawlerOptions and crawler engine
    
    * Add integration test for library
    
    * Remove bin
    
    * Update readme for code indentation
    
    * test domain update
    
    * workflow update
    
    * typo update
    
    Co-authored-by: sandeep <8293321+ehsandeep@users.noreply.github.com>

--- pkg/types/options.go
+++ pkg/types/options.go
@@ -4,7 +4,11 @@ import (
 	"strings"
 
 	"github.com/projectdiscovery/goflags"
+	"github.com/projectdiscovery/katana/pkg/output"
 )
+
+// OnResultCallback (output.Result)
+type OnResultCallback func(output.Result)
 
 type Options struct {
 	// URLs contains a list of URLs for crawling
@@ -83,6 +87,8 @@ type Options struct {
 	HeadlessOptionalArguments goflags.StringSlice
 	// HeadlessNoSandbox specifies if chrome should be start in --no-sandbox mode
 	HeadlessNoSandbox bool
+	// OnResult allows callback function on a result
+	OnResult OnResultCallback
 }
 
 func (options *Options) ParseCustomHeaders() map[string]string {
