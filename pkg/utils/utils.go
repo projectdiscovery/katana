3682bd62549786d4038de5f723007793a3469c54
geeknik <466878+geeknik@users.noreply.github.com>
2024-05-25 09:18:58 -0500

    Update utils.go
    fix spacing

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -65,7 +65,7 @@ func ParseRefreshTag(value string) string {
 
 // WebUserAgent returns the chrome-web user agent
 func WebUserAgent() string {
-    return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+        return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
 }
 
 func FlattenHeaders(headers map[string][]string) map[string]string {
