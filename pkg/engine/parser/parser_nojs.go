2c214680fc7202ef094556e35d67d00916dc1619
mavrick-1 <mavrickrishi@gmail.com>
2026-02-04 10:11:05 +0530

    fix: handle ScrapeJSLuiceResponses option in nocgo builds

--- pkg/engine/parser/parser_nojs.go
+++ pkg/engine/parser/parser_nojs.go
@@ -2,6 +2,16 @@
 
 package parser
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/projectdiscovery/katana/pkg/navigation"
+	"github.com/projectdiscovery/katana/pkg/utils"
+	stringsutil "github.com/projectdiscovery/utils/strings"
+)
+
 type Options struct {
 	AutomaticFormFill      bool
 	ScrapeJSLuiceResponses bool
@@ -13,6 +23,10 @@ func (p *Parser) InitWithOptions(options *Options) {
 	if options.AutomaticFormFill {
 		*p = append(*p, responseParser{bodyParser, bodyFormTagParser})
 	}
+	if options.ScrapeJSLuiceResponses {
+		*p = append(*p, responseParser{bodyParser, scriptContentJsluiceParser})
+		*p = append(*p, responseParser{contentParser, scriptJSFileJsluiceParser})
+	}
 	if options.ScrapeJSResponses {
 		*p = append(*p, responseParser{bodyParser, scriptContentRegexParser})
 		*p = append(*p, responseParser{contentParser, scriptJSFileRegexParser})
@@ -21,4 +35,37 @@ func (p *Parser) InitWithOptions(options *Options) {
 	if !options.DisableRedirects {
 		*p = append(*p, responseParser{headerParser, headerLocationParser})
 	}
+}
+
+// scriptContentJsluiceParser parses script content endpoints using pure-Go jsluice alternative
+func scriptContentJsluiceParser(resp *navigation.Response) (navigationRequests []*navigation.Request) {
+	resp.Reader.Find("script").Each(func(i int, item *goquery.Selection) {
+		text := item.Text()
+		if text == "" {
+			return
+		}
+
+		endpointItems := utils.ExtractJsluiceEndpoints(text)
+		for _, item := range endpointItems {
+			navigationRequests = append(navigationRequests, navigation.NewNavigationRequestURLFromResponse(item.Endpoint, resp.Resp.Request.URL.String(), "script", fmt.Sprintf("jsluice-%s", item.Type), resp))
+		}
+	})
+	return
+}
+
+// scriptJSFileJsluiceParser parses endpoints using pure-Go jsluice alternative from js file pages
+func scriptJSFileJsluiceParser(resp *navigation.Response) (navigationRequests []*navigation.Request) {
+	contentType := resp.Resp.Header.Get("Content-Type")
+	if !stringsutil.HasSuffixAny(resp.Resp.Request.URL.Path, ".js", ".css") && !strings.Contains(contentType, "/javascript") {
+		return
+	}
+	if utils.IsPathCommonJSLibraryFile(resp.Resp.Request.URL.Path) {
+		return
+	}
+
+	endpointsItems := utils.ExtractJsluiceEndpoints(string(resp.Body))
+	for _, item := range endpointsItems {
+		navigationRequests = append(navigationRequests, navigation.NewNavigationRequestURLFromResponse(item.Endpoint, resp.Resp.Request.URL.String(), "js", fmt.Sprintf("jsluice-%s", item.Type), resp))
+	}
+	return
 }
