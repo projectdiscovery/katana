e56c7d78d1affd488d7ad6c21770d69b2c4598b3
hriszc <510245979@qq.com>
2026-03-10 18:37:46 +0800

    Handle empty captured headless bodies

--- pkg/engine/hybrid/crawl_test.go
+++ pkg/engine/hybrid/crawl_test.go
@@ -2,6 +2,7 @@ package hybrid
 
 import (
 	"errors"
+	"net/http"
 	"testing"
 
 	"github.com/projectdiscovery/katana/pkg/navigation"
@@ -9,7 +10,10 @@ import (
 )
 
 func TestGetOutputBodyFallsBackToCapturedResponse(t *testing.T) {
-	resp := &navigation.Response{Body: "<html>fallback</html>"}
+	resp := &navigation.Response{
+		Resp: &http.Response{},
+		Body: "<html>fallback</html>",
+	}
 
 	body, err := getOutputBody(func() (string, error) {
 		return "", errors.New("could not get dom")
@@ -25,5 +29,19 @@ func TestGetOutputBodyErrorsWithoutFallback(t *testing.T) {
 	}, &navigation.Response{})
 
 	require.Error(t, err)
+	require.Empty(t, body)
+}
+
+func TestGetOutputBodyReturnsCapturedEmptyBody(t *testing.T) {
+	resp := &navigation.Response{
+		Resp: &http.Response{},
+		Body: "",
+	}
+
+	body, err := getOutputBody(func() (string, error) {
+		return "", errors.New("could not get dom")
+	}, resp)
+
+	require.NoError(t, err)
 	require.Empty(t, body)
 }
