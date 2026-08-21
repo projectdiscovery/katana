2736aebc4d5c446edad7f398005eb499eaf65f7f
BountyGrid <gcoinstash-cmd@users.noreply.github.com>
2026-08-21 15:06:14 -0700

    fix(types): synchronize ConfigureOutput to eliminate global logger data race (#1752)

---
+++ pkg/types/crawler_options_test.go
@@ -0,0 +1,52 @@
+package types
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestConcurrentConfigureOutput(t *testing.T) {
+	const goroutines = 50
+	var wg sync.WaitGroup
+	wg.Add(goroutines)
+
+	for i := 0; i < goroutines; i++ {
+		go func(idx int) {
+			defer wg.Done()
+			opts := &Options{
+				Silent:  idx%3 == 0,
+				Verbose: idx%3 == 1,
+				Debug:   idx%3 == 2,
+			}
+			opts.ConfigureOutput()
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func TestConcurrentNewCrawlerOptions(t *testing.T) {
+	const goroutines = 50
+	var wg sync.WaitGroup
+	wg.Add(goroutines)
+
+	for i := 0; i < goroutines; i++ {
+		go func(idx int) {
+			defer wg.Done()
+			opts := &Options{
+				MaxDepth:     1,
+				BodyReadSize: 1024,
+				Silent:       idx%3 == 0,
+				Verbose:      idx%3 == 1,
+				Debug:        idx%3 == 2,
+			}
+			co, err := NewCrawlerOptions(opts)
+			require.NoError(t, err)
+			_ = co.Close()
+		}(i)
+	}
+
+	wg.Wait()
+}
